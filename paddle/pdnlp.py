f08cd205f7c9644f4d62eac7ff8b991fb75be166
Artessay <rihong.edu@outlook.com>
2023-03-03 17:13:47 +0800

    json start

--- paddle/pdnlp.py
+++ paddle/pdnlp.py
@@ -2,6 +2,7 @@ from pprint import pprint
 from paddlenlp import Taskflow
 
 # Named Entity Recognition (NER)
-schema = ['Person', 'Company']
+schema = ['Player', 'Team', 'Time']
 ie_en = Taskflow('information_extraction', schema=schema, model='uie-base-en')
-print(ie_en('In 1997, Steve was excited to become the CEO.'))
+# print(ie_en('In 1997, Steve was excited to become the CEO.'))
+print(ie_en('The Utah Jazz have helped Lauri Markkanen unlock his offensive potential, becoming the kind of star they build a new foundation around.'))
