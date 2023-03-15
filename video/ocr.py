01b38c4b34079bdb627950e7bee3cb984edfe725
Artessay <rihong.edu@outlook.com>
2023-03-15 09:17:30 +0800

    modify over

--- video/ocr.py
+++ video/ocr.py
@@ -33,7 +33,7 @@ def text_process(img):
     # 时间处理
     text = texts[0]
     text = text.replace('：', ':')  # 防止识别为中文冒号
-    print(text)
+    # print(text)
     texts = text.split(':')
     print(texts)
 
@@ -41,25 +41,42 @@ def text_process(img):
         return (Clock(0, 0), 0)
 
     # 提取节
-    if texts[2].endswith('1ST'):
+    # if texts[2].endswith('1ST'):
+    #     period = 1
+    #     texts[2] = texts[2].rstrip('1ST')
+    # elif texts[2].endswith('2ND'):
+    #     period = 2
+    #     texts[2] = texts[2].rstrip('2ND')
+    # elif texts[2].endswith('3RD'):
+    #     period = 3
+    #     texts[2] = texts[2].rstrip('3RD')
+    # elif texts[2].endswith('4TH'):
+    #     period = 4
+    #     texts[2] = texts[2].rstrip('4TH')
+    # else:
+    #     return (Clock(0, 0), 0)
+    print(texts[0])
+    if texts[0].startswith('1ST'):
         period = 1
-        texts[2] = texts[2].rstrip('1ST')
-    elif texts[2].endswith('2ND'):
+        # texts[0] = texts[0].lstrip('1ST')
+    elif texts[0].startswith('2ND'):
         period = 2
-        texts[2] = texts[2].rstrip('2ND')
-    elif texts[2].endswith('3RD'):
+        # texts[0] = texts[0].lstrip('2ND')
+    elif texts[0].startswith('3RD'):
         period = 3
-        texts[2] = texts[2].rstrip('3RD')
-    elif texts[2].endswith('4TH'):
+        # texts[0] = texts[0].lstrip('3RD')
+    elif texts[0].startswith('4TH'):
         period = 4
-        texts[2] = texts[2].rstrip('4TH')
+        # texts[0] = texts[0].lstrip('4TH')
     else:
         return (Clock(0, 0), 0)
+    if (len(texts[0]) > 3):
+        texts[0] = texts[0][3:]
 
     if (texts[0] == ''):
         texts[0] = '0'
 
-    # print(texts[0], texts[1])
+    print(texts[0], texts[1])
     return (Clock(int(texts[0]), round(float(texts[1]))), period)
 
 if __name__ == '__main__':
