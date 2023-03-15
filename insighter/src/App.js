dee4744d91131d073548f3f160d274b329a8d588
Liqi Cheng <2896162191@qq.com>
2023-03-15 15:01:29 +0800

    fix

--- insighter/src/App.js
+++ insighter/src/App.js
@@ -3,6 +3,8 @@ import Head from './components/Head/Head';
 import Story from './components/Story/Story';
 import Data from './components/Data/Data'
 import Episode from './components/Episode/Episode'
+import StoryLine from './components/StoryLine/StoryLine'
+import View from './components/View/View'
 function App() {
   return (
     <div className='App'>
@@ -17,8 +19,12 @@ function App() {
             <div class="item item-3">
                 <Episode></Episode>
             </div>
-            <div class="item item-4">4</div>
-            <div class="item item-5">5</div>
+            <div class="item item-4">
+                {/* <View></View> */}
+            </div>
+            <div class="item item-5">
+            <StoryLine></StoryLine>
+            </div>
         </div>
     </div>
   );
