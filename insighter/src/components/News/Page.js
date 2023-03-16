04d1e8add67f5a5e6ea53a06d5c09d74d0fe76cd
Artessay <rihong.edu@outlook.com>
2023-03-16 10:35:38 +0800

    red

--- insighter/src/components/News/Page.js
+++ insighter/src/components/News/Page.js
@@ -1,3 +1,4 @@
+import './Page.css'
 import React from 'react';
 
 export default class Page extends React.Component {
@@ -11,7 +12,33 @@ export default class Page extends React.Component {
         
         return(
             <div className="Page">
-                hi
+                <div className='title'>
+                    <span></span>
+                    <img 
+                        src='./basketball.jpg' 
+                        height={50}
+                    ></img>
+                    <span>
+                        Joel Embiid's Shooting Distribution in the Game
+                    </span>
+                </div>
+                <div className='text'>
+                    <p>Joel Embiid most shots were taken from mid range and happened in first quarter. </p>
+
+                    <p>
+                        <span 
+                            style={{
+                                color: 'red'
+                            }}
+                        >
+                            by Writer's Name&nbsp;
+                        </span>
+                        <span>
+                            on Mar. 17, 2023
+                        </span>
+                    </p>
+                </div>
+                
             </div>
             
         )
