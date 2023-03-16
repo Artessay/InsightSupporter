f6ad35181ec08cafe5dbd3d58486e0f7c0cf2abf
Liqi Cheng <2896162191@qq.com>
2023-03-16 14:42:56 +0800

    communicata

--- insighter/src/App.js
+++ insighter/src/App.js
@@ -5,8 +5,33 @@ import Data from './components/Data/Data'
 import Episode from './components/Episode/Episode'
 import StoryLine from './components/StoryLine/StoryLine'
 import View from './components/View/View'
+import News from './components/News/News';
+import Test from '../src/data/Test.json' 
+import { useState } from 'react';
+//var Current_Episode =1;
 function App() {
+    var [DataSL,setDataSL] = useState(Test)
+    var [CE,setCE] = useState(1)
+    //var DataSL=Test;
+    //var Current_Episode =1;
+    var DataAll = (key,key2) => {
+        
+        
+        setCE(key+1)
+        var DataTmp1=DataSL
+        DataTmp1.Contents.Episode=(key2)
+        setDataSL(DataSL=DataTmp1)
+        console.log(DataSL)
+    };
+    var Datalist = (key) => {
+        var DataTmp=DataSL
+        DataTmp.Contents.Episode=(key)
+        setDataSL(DataSL=DataTmp)
+        //DataSL.Contents.Episode.filter
+        //console.log(DataSL)
+    };
   return (
+    
     <div className='App'>
         <Head className='header'></Head>
         <div className='content'>
@@ -17,17 +42,18 @@ function App() {
                 <Data></Data>
             </div>
             <div className="item item-3">
-                <Episode></Episode>
+                <Episode DataSL={DataSL} CE={CE}></Episode>
             </div>
-            <div class="item item-4">
-                {/* <View></View> */}
+            <div className="item item-4">
+            <News DataSL={DataSL}></News>
             </div>
-            <div class="item item-5">
-                <StoryLine></StoryLine>
+            <div className="item item-5">
+                <StoryLine DataSL={DataSL} DataAll={DataAll} Datalist={Datalist}></StoryLine>
             </div>
         </div>
     </div>
   );
 }
+
 
 export default App;
