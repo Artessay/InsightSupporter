27606cb9879fc5c04e96f1047ba5ab7b51abbcc7
Artessay <rihong.edu@outlook.com>
2023-03-16 22:22:14 +0800

    I don't know why too

--- insighter/src/components/charts/barchart.js
+++ insighter/src/components/charts/barchart.js
@@ -5,35 +5,66 @@ export default class BarChart extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            // 
+            data : [
+                { type: "Midrange", value: 11 },
+                { type: "3pt", value: 3 },
+                { type: "Layup", value: 2 },
+                { type: "Dunk", value: 3 }
+            ],
+            colors : ["#98abc5"]
         }
     }
 
     drawModel = () => {
-        const width = 300, height = 300;
-        const dataset = [34, 23, 54, 73, 88, 4];
-        const rectWidth = width / dataset.length;
+        let { data, colors } = this.state;
+
+        const width = 400, height = 300;
+        const left = 100, top = -30;
 
         let svg = d3.select('.barChart')
-                    .append('svg') 
-                    .attr('width', width)
-                    .attr('height', height);
+                    .append('svg')
+                    .attr('width',width)
+                    .attr('height',height)
+                    .attr("transform","translate("+left+","+0+")");
     
-        // y轴比例尺
-        let yScale = d3.scaleLinear()
-                    .range([height, 0])
-                    .domain([0, d3.max(dataset)]);
-    
-        svg.selectAll('rect')
-            .data(dataset)
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => rectWidth * i)
-            .attr('y', d => yScale(d))
-            .attr('width', rectWidth / 2)
-            .attr('height', d => {
-                return height - yScale(d);
-            });
+        // Define the x and y scales
+        var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1)
+            .domain(data.map(function(d) { return d.type; }));
+
+        var y = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        // Draw the x-axis
+        svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+        // Draw the y-axis
+        svg.append("g")
+        .call(d3.axisLeft(y));
+
+        // Draw the bars
+        svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.type); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .attr("fill", "#98abc5");
+
+        // Add title
+        svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Shots Categorized by Type");
     }
 
     componentDidMount() {
