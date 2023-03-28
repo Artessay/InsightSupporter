587b0fafc2533281a92b1af738f3db94837e1269
Artessay <rihong.edu@outlook.com>
2023-03-29 00:02:11 +0800

    add color

--- insighter/src/components/News/News.js
+++ insighter/src/components/News/News.js
@@ -1,9 +1,16 @@
-// import { Anchor, Col, Row } from 'antd';
-// import React, { useEffect, useState } from 'react';
 import React from 'react';
 import './News.css'
 import { Button } from 'antd';
 import Page from './Page';
+
+const taskColor = {
+    'Value': '#9747FF',
+    'Proportion': '#FEB563',
+    'Distribution': '#B3DE69',
+    'Categorization': '#8DD3C7',
+    'Trend': '#EEFF00',
+    'Difference': '#22EEFF'
+};
 
 export default class News extends React.Component {
     constructor(props) {
@@ -33,6 +40,11 @@ export default class News extends React.Component {
 
         const listLength = DataSL.Contents.Episode.length;
         const numbers = Array.from({ length: listLength }, (_, index) => index);
+
+        numbers.map((number) => (
+            console.log(DataSL.Contents.Episode[number].E_Sentences[0].S_Task)
+            // console.log(taskColor[DataSL.Contents.Episode[number].E_Sentences[0].S_Task]);
+        ))
         
         return(
             <div className="News">
@@ -42,12 +54,15 @@ export default class News extends React.Component {
                     </div>
                     
                     <div className='buttons'>
-                        <Button shape='circle' onClick={(e) => this.handleClick(e)} value="0">0</Button>
+                        <Button shape='circle' onClick={(e) => this.handleClick(e)} value="0"
+                        style={{backgroundColor: 'rgba(117, 117, 117, 0.3)'}}>0</Button>
                         {
                             numbers.map((number) => (
                                 < >
                                     <div className='verticalLine'></div>
-                                    <Button key={number} shape='circle' onClick={(e) => this.handleClick(e)} value={number+1}>{number+1}</Button>
+                                    <Button key={number} 
+                                    style={{backgroundColor: taskColor[DataSL.Contents.Episode[number].E_Sentences[0].S_Task]}}
+                                    shape='circle' onClick={(e) => this.handleClick(e)} value={number+1}>{number+1}</Button>
                                 </>
                             ))
                         }
